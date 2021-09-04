8ba618601621df8fae6faa9d39615082ec4ee45f
Fombi Favour <fombifavour@gmail.com>
2021-09-04 01:10:52 +0100

    switching scenes

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,8 +6,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -28,18 +29,35 @@ public class Main extends Application {
 
     public Scene simpleScene(Stage stage){// Creating a button in the first scene to be placed at the center
         Button simpleButton = new Button();
+        Button anotherButton = new Button();
         simpleButton.setPrefWidth(100);
         simpleButton.setPrefHeight(20);
-        simpleButton.setText("Next");
 
-        simpleButton.setAlignment(Pos.CENTER);
+        anotherButton.setPrefWidth(100);
+        anotherButton.setPrefHeight(20);
+
+
+        simpleButton.setText("Next");
+        anotherButton.setText("Data");
+
+        HBox hbox1 = new HBox(simpleButton, anotherButton);
+        hbox1.setSpacing(10);
+
+
+        hbox1.setAlignment(Pos.CENTER);
+
 
         BorderPane simplePane = new BorderPane();// It is used in order to set it at the center
-        simplePane.setCenter(simpleButton);
+        simplePane.setCenter(hbox1);
 
         simpleButton.setOnAction(actionEvent -> {
             stage.close();
             stage.setScene(borderScene(stage));
+            stage.show();
+        });
+        anotherButton.setOnAction(actionEvent -> {
+            stage.close();
+            stage.setScene(accountScene(stage));
             stage.show();
         });
 
@@ -48,11 +66,14 @@ public class Main extends Application {
     public Scene borderScene(Stage stage){// Displays the borders: home, close, add, sub
         Button addButton = new Button("Add"); addButton.setPrefHeight(stage.getHeight());// Adjusting the width on add
         Button subButton = new Button("Sub"); subButton.setPrefHeight(stage.getHeight());// and sub button
-        Button closeButton = new Button("Close"); closeButton.setPrefWidth(stage.getWidth());// Adjusting the height on the close and
+        Button closeButton = new Button("Close"); closeButton.setPrefWidth(stage.getWidth()/2);// Adjusting the height on the close and
+        Button resetButton = new Button("Reset"); resetButton.setPrefWidth(stage.getWidth()/2);// reset button adjacent to each other
         Button homeButton = new Button("Home"); homeButton.setPrefWidth(stage.getWidth());// home button
 
+        HBox hbox = new HBox(closeButton, resetButton);
+
         BorderPane borderPane = new BorderPane(); //Adjusting the position of the various buttons
-        borderPane.setBottom(closeButton);// at the bottom
+        borderPane.setBottom(hbox);// at the bottom
         borderPane.setLeft(addButton);// on the left
         borderPane.setRight(subButton);// on the right
         borderPane.setTop(homeButton);// at the top
@@ -80,6 +101,35 @@ public class Main extends Application {
             stage.show();
         });
 
+        resetButton.setOnAction(actionEvent -> {
+            int ret = Integer.parseInt(label.getText());
+            label.setText("0");
+
+        });
+
         return new Scene(borderPane);
+    }
+
+    public Scene accountScene(Stage stage){// entering to the next scene to enter any text
+
+        Label label = new Label();
+        label.setText("Enter any text: ");
+
+        TextField inputText = new TextField();// text to be inputed
+
+        Button btn = new Button("Display");
+
+        Label label1 = new Label();// text to be displayed
+
+        HBox side = new HBox(label, inputText, btn, label1);
+
+        BorderPane borderPane1 = new BorderPane();
+        borderPane1.setCenter(side);
+
+        btn.setOnAction(actionEvent -> {
+            label1.setText(inputText.getText());
+        });
+
+        return new Scene(borderPane1);
     }
 }
